ee19afcd9bdc2af9d7db842c2ae9432369bf5312
James Harradon <james.harradon@gmail.com>
2021-05-12 16:52:09 +0100

    Intial commit

---
+++ main.js
@@ -0,0 +1,267 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+// const EventEmitter = require("events");
+// const emitter = new EventEmitter;
+
+// const keypress = require("keypress");
+// keypress(process.stdin);
+
+const hat = '^';
+const hole = 'O';
+const fieldCharacter = '░';
+const pathCharacter = '*';
+let isEndOfGame = false;
+let moveCounter = 0; // to change psn of hat after every 10 moves.
+let startTime;
+let endTime;
+let h;
+let w;
+
+//how to make the game better ;
+// - 
+// - 
+//  codecademy ideas ;
+// - 
+// - add more holes after a certain time
+// - improve your games graphics??
+// - create a field validator to check whether the game can actually be solved.
+
+
+class Field {
+    constructor(arr = [[]]) {
+        this.arr = arr;
+        this.row = 0;
+        this.column = 0;  
+    }
+    static generateField(width, height, percOfHoles) {
+        // was really struggling creating this 2d array, code below copied from codecademy solution, creates a 2d array with height x rows and width x columns.
+        
+        // ive read that you should avoid using new Array. let arr = new Array(10, 20, 30) would create a new array filled with those values, however using just one value eg 10, will produce an array with 10 empty elements. This can help us in this case.
+        let arr = new Array(height).fill(0).map(el => new Array(width));
+        //randomise function
+        const randomise = (num) => {
+            return Math.floor(Math.random()*num)
+        }
+        
+        //fills field with fieldCharacters
+        for (let i = 0; i < height; i++) {
+            for (let j = 0; j < width; j++) {
+                arr[i][j] = fieldCharacter;
+            }
+        }
+        // randomly places holes
+        
+        let numOfHoles = (width * height)/100 * percOfHoles;
+        while(numOfHoles > 0) {
+            arr[randomise(height)][randomise(width)] = hole;
+            numOfHoles--;
+        } 
+        // puts hat at top left of grid
+        arr[0][0] = pathCharacter;
+
+        // randomises location of hat-
+        arr[randomise(height)][randomise(width)] = hat;
+        
+
+        //the below code in the print() method doesnt seem to work, i think it is because the method cannot access the arr variable
+        return arr;
+        
+    }
+
+    randomiseHat(height, width) {
+        outer:for (let i = 0; i < height; i++) {
+            inner:for (let j = 0; j < width; j++) {
+                if (this.arr[i][j] === hat) {
+                    this.arr[i][j] = fieldCharacter;
+                    break outer; //breaks out of outer loop once hat is found.
+                }
+            }
+        }
+        this.arr[Math.floor(Math.random()*height)][Math.floor(Math.random()*width)] = hat;
+    }
+
+    fieldValidator() {
+        //tests whether field can be solved
+
+        // 1 - produce all co-ordinates of holes - no go area
+        // 2 - produce all co-ordinates of outofbounds - no go area
+        // 3 - give co-ordinates of hat
+        // 4 - move into empty field space If(arr[i][j] === 'field character'); generated arr[i][j] will have to be within ONE move of the current psn.
+        // 5 - record own trail as no go area
+        //
+
+    }
+
+
+    print() {
+        // prints array as a string to the console, this will need to be done after each move.
+        this.arr.forEach(row => {
+            console.log(row.join(""));
+        })
+    }
+
+    getPosition() {
+       // return the current position of the player;
+        return this.arr[this.row][this.column];
+    }
+
+
+    move() {
+        
+        let validDir = false;
+
+        while(!validDir){
+            let dir = prompt(`Which way do you want to move? Please type either l, r, u or d :`);
+        
+            if(dir === "l") {
+                this.column -=1;
+                validDir = true;
+                
+            } else if (dir === "r") {
+                this.column +=1;
+                validDir = true;
+                
+            } else if(dir ==="u") {
+                this.row -=1;
+                validDir = true;
+                
+            } else if (dir === "d") {
+                this.row +=1;
+                validDir = true;
+                
+            }else {
+                // this msg doesnt show as it goes straight to the prompt
+                "Please enter a valid direction!";
+            }
+
+        }
+        
+        
+
+
+        // the below is for using the arrow keys to move but it isnt working atm.
+        // process.stdin.on("keypress", keyListener);
+    
+        // const keyListener = (e) => {
+        //     return e.keyCode;
+        // }
+        // //changes psn of fieldCharacter using arrow keys.
+        // // ensures user cannot move outside area.
+        
+        // switch(keyListener) {
+        //     case 37: this.row -= 1;
+        //              break;
+            
+        //     case 38: this.column -= 1;
+        //              break;
+
+        //     case 39: this.row += 1;
+        //              break;
+
+        //     case 40: this.column += 1;
+        //              break;
+        
+        //     default:
+        //     return;
+        // }
+        
+    }
+
+    assessMove() {
+    
+        //if pathCharacter goes out of game area, game is over.
+        if(this.column < 0 ||this.column > this.arr[0].length -1 || this.row < 0 || this.row > this.arr.length -1)  {
+            isEndOfGame = true;
+            console.log("You have moved out of the game area - GAME OVER!");
+        }
+        
+        // if pathCharacter encounters another pathCharacter(its own trail) it cannot move
+        else if(this.getPosition() === pathCharacter) {
+            isEndOfGame = true;
+            console.log("You have hit your own trail - GAME OVER!");
+        }
+
+        //if pathCharacter encounters a O, console.log("You fell down a hole, unlucky")
+        else if(this.getPosition() === hole) {
+            isEndOfGame = true;
+            console.log("You have fallen down a hole - GAME OVER!");
+        }
+
+        //if pathCharacter finds the ^, console.log("You found your hat, well done! You completed the game")
+        else if (this.getPosition() === hat) {
+            isEndOfGame = true;
+            this.getMyTime("end")
+            let points =  100000 - (endTime - startTime);
+            console.log("You have found your hat! Well done! You completed the game.");
+            console.log(`You scored ${points} points!`);
+        } else {
+            moveCounter++;
+            this.arr[this.row][this.column] = pathCharacter; // doesnt like this.getPosition() = pathCharacter for some reason.
+        }
+    }
+
+    getMyTime(time) {
+        // provides a start and end time to calculate a score.
+        if(time === "start") {
+            startTime = new Date().getTime();
+        } else if (time === "end") {
+            endTime = new Date().getTime();
+        }
+    }
+
+    playAgain(name) {
+        let playAgainDecisionMade = false;
+
+        while(!playAgainDecisionMade) {
+        let playAgain = prompt(`${name} thanks for playing, would you like to play again? Please type either y or n.`);
+        if(playAgain === "y") {
+            playGame();
+            playAgainDecisionMade = true;
+        } else if (playAgain === "n"){
+            playAgainDecisionMade = true;
+            console.log(`Bye ${name}!`);
+        } 
+        }
+    }
+}
+
+function playGame() {
+    
+    isEndOfGame = false;
+    let name = prompt("Hi What is your name? :");
+    w = prompt(`${name}, please type the width you'd like you game area to be.`);
+    h = prompt(`Now please type the height.`);
+    const myField = new Field(Field.generateField(w, h, 20));
+    myField.print();
+    console.log(`Hi ${name}, lets play Find Your Hat!`);
+    let hardMode = prompt(`${name}, would you like to play hard mode? Please type either y or n`)
+    console.log(`Move the * to find the ^ avoiding the O's, and stay in the game area! The quicker you find your hat, the more points you get....GO!`);
+    myField.getMyTime("start");
+    while(!isEndOfGame) {
+        myField.move()
+        myField.assessMove()
+        if (hardMode === "y") {
+            if(moveCounter % Math.ceil(Math.random()*10) === 0) {
+                myField.randomiseHat(myField.arr.length, myField.arr[0].length);
+            }
+        } else if(hardMode === "n") {
+            if(moveCounter % 10 === 0) {
+                myField.randomiseHat(myField.arr.length, myField.arr[0].length);
+            }
+        }
+        
+        if(!isEndOfGame) {
+            myField.print();
+        }
+    }
+
+    setTimeout(function(){myField.playAgain(name)}, 2000); // something i found out, when using setTimeout with a function, the function you want to run, in this case playAgain() has to go inside another function!
+    
+
+}
+
+
+
+
+playGame();
+
